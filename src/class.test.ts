b41413c0f21683a9d3fc39096186a366696c53f3
Ivan Duran <dureivan@gmail.com>
2023-01-30 20:10:32 +0100

    Add test for King class

--- src/class.test.ts
+++ src/class.test.ts
@@ -1,11 +1,35 @@
-import { Character } from './class';
-
+import { Character } from './Personaje';
+import { King } from './Rey';
 describe('Given the class Character', () => {
   describe('When pass the name, family and age properties', () => {
     test('Then it should return an object with the same properties and the values given', () => {
       const r = new Character('Eddard', 'Stark', 42);
       const result = { name: 'Eddard', family: 'Stark', age: 42, alive: true };
       expect(r).toEqual(result);
+    });
+  });
+});
+
+describe('Given the class King', () => {
+  describe('When pass the name, family, age and kingYears properties', () => {
+    test('Then it should return an object with the same properties and the values given', () => {
+      const r = new King('Robert', 'Baratheon', 50, 12);
+      const result = {
+        name: 'Robert',
+        family: 'Baratheon',
+        age: 50,
+        alive: true,
+        kingYears: 12,
+      };
+      expect(r).toEqual(result);
+    });
+  });
+
+  describe('When a King item uses its method talk', () => {
+    test('Then it should return "Vais a morir todos"', () => {
+      const robert = new King('Robert', 'Baratheon', 50, 12);
+      const r = robert.talk();
+      expect(r).toBe('Vais a morir todos');
     });
   });
 });
