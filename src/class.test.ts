c092626b2f7d86037eb3fd8372259976d8410970
Ivan Duran <dureivan@gmail.com>
2023-01-30 19:51:31 +0100

    Add test for class Character

---
+++ src/class.test.ts
@@ -0,0 +1,11 @@
+import { Character } from './class';
+
+describe('Given the class Character', () => {
+  describe('When pass the name, family and age properties', () => {
+    test('Then it should return an object with the same properties and the values given', () => {
+      const r = new Character('Eddard', 'Stark', 42);
+      const result = { name: 'Eddard', family: 'Stark', age: 42, alive: true };
+      expect(r).toEqual(result);
+    });
+  });
+});
